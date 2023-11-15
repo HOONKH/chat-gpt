30ab9a0be79273a3b2678f68ea0471eee2763ad2
HOONKH <boospa0305@gmai.com>
2023-11-15 10:06:07 +0900

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="bg-purple-300">Hello, world</div>;
+  return (
+    <div className=" min-h-screen max-w-screen-md mx-auto flex">
+      <div className="bg-amber-200 w-52">Side</div>
+      <div className="grow flex flex-col">
+        <div className="bg-green-200 grow">Main</div>
+        <div className="bg-blue-200 h-20">Chat Bar</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
