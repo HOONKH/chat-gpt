24bbb5c567ee7ac7f4585f6ec7562e6751a6053b
HOONKH <boospa0305@gmai.com>
2023-11-19 21:16:25 +0900

    howtodelete

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import { useEffect, useState } from "react";
 import ChatBar from "./components/ChatBar";
 import ChatView from "./components/ChatView";
 import ChatSide from "./components/ChatSide";
-import LocalAnswer from "./components/LocalAnswer";
 
 const App = () => {
-  const [prevAnswer, setPrevAnswer] = useState([]);
-
   const getChatList = () => {
     const localChatList = localStorage.getItem("questions");
 
@@ -15,17 +12,16 @@ const App = () => {
 
     const parsedChatList = JSON.parse(localChatList);
 
-    setPrevAnswer(parsedChatList);
+    setChatList(parsedChatList);
   };
   const [chatList, setChatList] = useState([]);
 
   useEffect(() => {
     getChatList();
-    console.log(prevAnswer);
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-900 relative">
+    <div className="min-h-screen bg-gray-900 relative flex">
       <div className="absolute">
         <img
           className="fixed rounded-xl h-[920px]"
@@ -38,12 +34,6 @@ const App = () => {
         <div className="grow flex flex-col relative">
           <ChatView chatList={chatList} />
           <ChatBar chatList={chatList} setChatList={setChatList} />
-        </div>
-
-        <div className="z-50 gap-2 mb-2 flex flex-col justify-end max-h-screen items-end max-w-screen ">
-          {prevAnswer.map((v, i) => {
-            return <LocalAnswer key={i} prevAnswer={v} />;
-          })}
         </div>
       </div>
     </div>
--- src/components/ChatSideCard.jsx
+++ src/components/ChatSideCard.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
+import { ImBin } from "react-icons/im";
 
 const ChatSideCard = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,19 +14,22 @@ const ChatSideCard = ({ question, answer }) => {
   return (
     <Fragment>
       <li
-        className="mb-1 truncate cursor-pointer hover:font-bold mt-3"
+        className="mb-1 truncate cursor-pointer hover:font-bold mt-3 flex items-center "
         onClick={onClickModal}
       >
         {question}
       </li>
       {isOpen && (
         <div className="fixed top-0 left-0 text-white bg-black w-full h-full z-30 opacity-70 ">
-          <div className="rounded-md bg-blue-900 fixed p-4 w-[1000px] h-[700px] top-1/4 left-1/3 text-[15px] translate-x-1/7 translate-y-1/6 whitespace-pre-wrap overflow-y-auto">
+          <div className="rounded-md bg-blue-900 fixed p-4 w-[1000px] h-[700px] top-1/8 left-1/4 text-[15px] translate-x-1/5 translate-y-1 whitespace-pre-wrap overflow-y-auto">
             <button
               className="hover:bg-blue-700 active:bg-blue-900 rounded-md text-2xl "
               onClick={onClickClose}
             >
               <FiArrowLeft />
+            </button>
+            <button>
+              <ImBin className="ml-3 text-lg mb-0.5" />
             </button>
             <div>Q. {question}</div>
             <div>A. {answer}</div>
